37aa0b409b788fd555536c1ea7413fdd30e9f6c3
PkMs7 <patrickmarques2009@hotmail.com>
2024-11-08 17:40:17 -0300

    style: :lipstick: Adiciona estilização global para a aplicação

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import App from './pages/index.jsx'
+import GlobalStyles from './styles.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <GlobalStyles />
     <App />
   </StrictMode>,
 )
